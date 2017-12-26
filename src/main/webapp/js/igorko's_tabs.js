6bad5ba4dd8d9d313a10504986eced23eff50fea
IgorkoGR <55022@mail.ru>
2017-12-26 13:50:55 +0200

    Admin page2 (#9)
    * Test
    
    * Ok, this time no conflicts should appear

---
+++ src/main/webapp/js/igorko's_tabs.js
@@ -0,0 +1,47 @@
+function openTab(evt, tabName) 
+{
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab_content");
+    for (i = 0; i < tabcontent.length; i++) 
+	{
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab_button");
+    for (i = 0; i < tablinks.length; i++) 
+	{
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openTabLink(evt, tabName) 
+{
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab_link_content");
+    for (i = 0; i < tabcontent.length; i++) 
+	{
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab_link");
+    for (i = 0; i < tablinks.length; i++) 
+	{
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
